96f28781625cb5747069afc202546fd3473e72a3
Mohd Abid <abidraeen05@gmail.com>
2026-02-24 19:20:21 +0530

    change database name

--- src/config/db_connect.js
+++ src/config/db_connect.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constant.js";
 
 const db_connect = async () => {
   try {
-    const conn = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`✅ MongoDB connected: ${conn.connection.host} ${conn.connection.name}`);
-    
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err.message);
     process.exit(1);
